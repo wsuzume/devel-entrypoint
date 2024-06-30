8681b49c1ce2795231c9178aeb87705aa7e41f2e
wsuzume <joshnobus@gmail.com>
2024-06-30 20:44:45 +0900

    change env var name

--- init.sh
+++ init.sh
@@ -27,102 +27,102 @@ else
 fi
 
 # Default user settings
-if [ -z "${USER}" ]; then
-    USER="morgan"
+if [ -z "${INIT_USER}" ]; then
+    INIT_USER="morgan"
 fi
-if [ -z "${UID}" ]; then
-    UID="1000"
+if [ -z "${INIT_UID}" ]; then
+    INIT_UID="1000"
 fi
-if [ -z "${GID}" ]; then
-    GID="100"
+if [ -z "${INIT_GID}" ]; then
+    INIT_GID="100"
 fi
 
 # If the container started as the root user, then we have permission to refit
 # the morgan user, and ensure file permissions, grant sudo rights, and such
 # things before we run the command passed to start.sh as the desired user
-# (USER).
+# (INIT_USER).
 #
 if [ "$(id -u)" == 0 ]; then
     # Environment variables:
-    # - USER: the desired username and associated home folder (default "morgan")
-    # - UID: the desired user id (default "1000")
-    # - GID: a group id we want our user to belong to (default "100")
-    # - GROUP: a group name we want for the group (default the same with USER)
+    # - INIT_USER: the desired username and associated home folder (default "morgan")
+    # - INIT_UID: the desired user id (default "1000")
+    # - INIT_GID: a group id we want our user to belong to (default "100")
+    # - INIT_GROUP: a group name we want for the group (default the same with INIT_USER)
     # - GRANT_SUDO: a boolean ("1" or "yes") to grant the user sudo rights
     # - CHOWN_HOME: a boolean ("1" or "yes") to chown the user's home folder
     # - CHOWN_EXTRA: a comma-separated list of paths to chown
     # - CHOWN_HOME_OPTS / CHOWN_EXTRA_OPTS: arguments to the chown commands
 
     # Group configuration
-    if [[ -z "${GROUP}" && -n "${GID}" ]]; then
-        # Only GID is specified
-        if getent group | grep -q ":${GID}:"; then
+    if [[ -z "${INIT_GROUP}" && -n "${INIT_GID}" ]]; then
+        # Only INIT_GID is specified
+        if getent group | grep -q ":${INIT_GID}:"; then
             # The group exists
-            GROUP=$(getent group | grep ":100:" | cut -d: -f1)
-            _log "Use existing group: '${GROUP}' (${GID})"
+            INIT_GROUP=$(getent group | grep ":100:" | cut -d: -f1)
+            _log "Use existing group: '${INIT_GROUP}' (${INIT_GID})"
         else
-            GROUP="${USER}"
-            groupadd --gid "${GID}" "${GROUP}"
-            _log "Create specified group: '${GROUP}' (${GID})"
+            INIT_GROUP="${INIT_USER}"
+            groupadd --gid "${INIT_GID}" "${INIT_GROUP}"
+            _log "Create specified group: '${INIT_GROUP}' (${INIT_GID})"
         fi
     else
-        # Both GROUP and GID specified
-        if getent group "${GROUP}" &> /dev/null; then
-            # Search GID from GROUP and it exists
-            existing_gid=$(getent group ${GROUP} | cut -d: -f3)
-            if [ "${existing_gid}" != "${GID}" ]; then
-                _log "ERROR: When searching by GROUP, the group '${GROUP} (${GID})' already exists, but the specified GID '${GID}' does not match."
+        # Both INIT_GROUP and INIT_GID specified
+        if getent group "${INIT_GROUP}" &> /dev/null; then
+            # Search INIT_GID from INIT_GROUP and it exists
+            existing_gid=$(getent group ${INIT_GROUP} | cut -d: -f3)
+            if [ "${existing_gid}" != "${INIT_GID}" ]; then
+                _log "ERROR: When searching by INIT_GROUP, the group '${INIT_GROUP} (${INIT_GID})' already exists, but the specified INIT_GID '${INIT_GID}' does not match."
                 exit 1
             else
                 # Both informations are correct
-                _log "Use existing group: '${GROUP}' (${GID})"
+                _log "Use existing group: '${INIT_GROUP}' (${INIT_GID})"
             fi
-        elif getent group | grep -q ":${GID}:"; then
-            # Search GROUP from GID and it exists
+        elif getent group | grep -q ":${INIT_GID}:"; then
+            # Search INIT_GROUP from INIT_GID and it exists
             existing_gname=$(getent group | grep ":100:" | cut -d: -f1)
-            if [ "${existing_gname}" != "${GROUP}" ]; then
-                _log "ERROR: When searching by GID, the group '${GROUP} (${GID})' already exists, but the specified GROUP '${GROUP}' does not match."
+            if [ "${existing_gname}" != "${INIT_GROUP}" ]; then
+                _log "ERROR: When searching by INIT_GID, the group '${INIT_GROUP} (${INIT_GID})' already exists, but the specified INIT_GROUP '${INIT_GROUP}' does not match."
                 exit 1
             else
                 # Both informations are correct
-                _log "Use existing group: '${GROUP}' (${GID})"
+                _log "Use existing group: '${INIT_GROUP}' (${INIT_GID})"
             fi
         else
             # The group does not exist
             # Create the desired group
-            groupadd --gid "${GID}" "${GROUP}"
-            _log "Create specified group: '${GROUP}' (${GID})"
+            groupadd --gid "${INIT_GID}" "${INIT_GROUP}"
+            _log "Create specified group: '${INIT_GROUP}' (${INIT_GID})"
         fi
     fi
 
     # User configuration
-    if id "${USER}" &> /dev/null; then
-        # Search UID by USER
-        existing_uid=$(id -u "${USER}")
-        if [ "${existing_uid}" != "${UID}" ]; then
-            _log "ERROR: When searching by USER, the user '${USER} (${existing_uid})' already exists, but the specified UID '${UID}' does not match."
+    if id "${INIT_USER}" &> /dev/null; then
+        # Search INIT_UID by INIT_USER
+        existing_uid=$(id -u "${INIT_USER}")
+        if [ "${existing_uid}" != "${INIT_UID}" ]; then
+            _log "ERROR: When searching by INIT_USER, the user '${INIT_USER} (${existing_uid})' already exists, but the specified INIT_UID '${INIT_UID}' does not match."
             exit 1
         else
             # Both informations are correct
-            _log "Use existing user: '${USER}' (${UID})"
+            _log "Use existing user: '${INIT_USER}' (${INIT_UID})"
         fi
     else
-        # Search USER by UID
-        existing_uname=$(getent passwd | awk -F: -v uid="${UID}" '$3 == uid {print $1}')
+        # Search INIT_USER by INIT_UID
+        existing_uname=$(getent passwd | awk -F: -v uid="${INIT_UID}" '$3 == uid {print $1}')
         if [ -n "{$existing_uname}" ]; then
-            if [ "${existing_uname}" != "${USER}" ]; then
-                _log "UID=${UID}"
-                _log "ERROR: When searching by UID, the user '${existing_uname} (${UID})' already exists, but the specified USER '${USER}' does not match."
+            if [ "${existing_uname}" != "${INIT_USER}" ]; then
+                _log ""
+                _log "ERROR: When searching by INIT_UID, the user '${existing_uname} (${INIT_UID})' already exists, but the specified INIT_USER '${INIT_USER}' does not match."
                 exit 1
             else
                 # Both informations are correct
-                _log "Use existing user: '${USER}' (${UID})"
+                _log "Use existing user: '${INIT_USER}' (${INIT_UID})"
             fi
         else
             # The user does not exist
             # Create the desired user
-            useradd --no-log-init -m --home "/home/${USER}" --shell /bin/bash --uid "${UID}" --gid "${GID}" --groups 100 "${USER}"
-            _log "Create specified user: '${USER}' (${UID})"
+            useradd --no-log-init -m --home "/home/${INIT_USER}" --shell /bin/bash --uid "${INIT_UID}" --gid "${INIT_GID}" --groups 100 "${INIT_USER}"
+            _log "Create specified user: '${INIT_USER}' (${INIT_UID})"
         fi
     fi
 else
